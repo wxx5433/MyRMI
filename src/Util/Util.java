6ef7fb18df26c0192a835050c70b496f5bf0ca3a
wxx5433 <wxx5433@gmail.com>
2014-10-05 15:47:02 -0400

    Finish the createProxy method

--- src/Util/Util.java
+++ src/Util/Util.java
@@ -3,19 +3,19 @@ package Util;
 import java.lang.reflect.Proxy;
 
 import Remote.Remote640;
-import Remote.RemoteObjectReference;
 
 public abstract class Util {
 	/**
-	 * Create a proxy here. 
-	 * @param impl  The object that directly or indirectly implements Remote640 interface
-	 * @param clientRef
-	 * @return
+	 * Create a proxy here.
+	 * When the client call a remote method,
+	 * the proxy sends the remote invocation message to the server. 
+	 * @param stub  The stub object that directly or indirectly implements Remote640 interface
+	 * @return the proxy
 	 */
-	public static Remote640 createProxy(Object impl, RemoteObjectReference clientRef) {
-		RemoteObjectInvocationHandler handler = new RemoteObjectInvocationHandler(impl);
-		Remote640 proxy = (Remote640) Proxy.newProxyInstance(impl.getClass().getClassLoader(),
-				impl.getClass().getInterfaces(), handler);
+	public static Remote640 createProxy(Stub stub) {
+		RemoteObjectInvocationHandler handler = new RemoteObjectInvocationHandler(stub);
+		Remote640 proxy = (Remote640) Proxy.newProxyInstance(stub.getClass().getClassLoader(),
+				stub.getClass().getInterfaces(), handler);
 		return proxy;
 	}
 }
