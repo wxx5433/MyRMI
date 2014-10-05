8cfe45f43c9a45f0b43d72607efa0861e6b137e6
wxx5433 <wxx5433@gmail.com>
2014-10-05 18:25:54 -0400

    add sendRemoteCallRequest&getRemoteCallResponse

--- src/Util/Util.java
+++ src/Util/Util.java
@@ -1,7 +1,13 @@
 package Util;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Proxy;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+import Communication.RMIMessage;
 import Remote.Remote640;
 import Stub.Stub;
 
@@ -18,5 +24,45 @@ public abstract class Util {
 		Remote640 proxy = (Remote640) Proxy.newProxyInstance(stub.getClass().getClassLoader(),
 				stub.getClass().getInterfaces(), handler);
 		return proxy;
+	}
+	
+	/**
+	 * Send a remote call to the server running the service. 
+	 * @param socket socket connection with the server
+	 * @param message the message to send
+	 */
+	public static void sendRemoteCallRequest(Socket socket, RMIMessage message) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+			out.writeObject(message);
+			out.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * receive the result of a remote call from the server.
+	 * @param socket socket connection with the server
+	 * @return return value from the remote functino call.
+	 */
+	public static RMIMessage getRemoteCallResponse(Socket socket) {
+		RMIMessage response = null;
+		try {
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			response = (RMIMessage) in.readObject();
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return response;
 	}
 }
