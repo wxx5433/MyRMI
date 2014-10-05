496925b6260e10f346ef6a34fd18dfd92af5858a
wxx5433 <wxx5433@gmail.com>
2014-10-05 13:14:32 -0400

    Add remote Interface

---
+++ src/Remote/Remote640.java
@@ -0,0 +1,11 @@
+package Remote;
+
+/**
+ * Only when an interface extends Remote640 can its methods be called remotely.
+ * Any object that is a remote object must directly or indirectly implement this interface. 
+ * 
+ * @author Xiaoxiang Wu (xiaoxiaw)
+ * @author Ye Zhou (zhouye)
+ *
+ */
+public interface Remote640 { }
