b837cefa01db51291db733adb48a264db2a31c39
wxx5433 <wxx5433@gmail.com>
2014-10-06 20:36:59 -0400

    add RemoteException

---
+++ src/Exception/RemoteException.java
@@ -0,0 +1,11 @@
+package Exception;
+
+public class RemoteException extends Exception {
+
+	private static final long serialVersionUID = -7264232105421295868L;
+	
+	public RemoteException(String message) {
+		super(message);
+	}
+
+}
