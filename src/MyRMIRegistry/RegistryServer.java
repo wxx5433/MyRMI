80e20fb12edd16bd22a604e0b111171f7a1090e0
wxx5433 <wxx5433@gmail.com>
2014-10-10 14:43:09 -0400

    add objectKey to addService and change objectKey from int to long.

--- src/MyRMIRegistry/RegistryServer.java
+++ src/MyRMIRegistry/RegistryServer.java
@@ -25,7 +25,7 @@ public class RegistryServer {
 	 * 1st map:  Key: serviceName,  Value: 2nd map
 	 * 2nd map:  Key: server running this service(may exist several servers), Value: ObjectKey 
 	 */
-	private Map<String, Map<NodeID, Integer>> remoteObjectTable = null;
+	private Map<String, Map<NodeID, Long>> remoteObjectTable = null;
 	
 	private String hostName;
 	private int portNum;    // the port to listening for incoming requests
@@ -43,7 +43,7 @@ public class RegistryServer {
 		this.portNum = portNum;
 		socketListener = null;
 		listen = null;
-		remoteObjectTable = new ConcurrentHashMap<String, Map<NodeID, Integer>>();
+		remoteObjectTable = new ConcurrentHashMap<String, Map<NodeID, Long>>();
 	}
 
 
@@ -54,14 +54,18 @@ public class RegistryServer {
 	 * @param port dispatch server's listening port
 	 * @return message to the dispatch server to tell if the request is successful
 	 */
-	public CommunicationMessage addService(String serviceName, String ip, int port) {
+	public CommunicationMessage addService(String serviceName, String ip, int port, long objectKey) {
 		// new serviceName
 		CommunicationMessage replyMessage = null;
 		NodeID nodeID = new NodeID(ip, port);
-		Map<NodeID, Integer> services = null;
+		Map<NodeID, Long> services = null;
 		if (!remoteObjectTable.containsKey(serviceName)) {
-			services = new ConcurrentHashMap<NodeID, Integer>();
-			services.put(nodeID, 0);
+			services = new ConcurrentHashMap<NodeID, Long>();
+			if (objectKey != -1) {
+				services.put(nodeID, objectKey);
+			} else {
+				services.put(nodeID, 0L);
+			}
 			remoteObjectTable.put(serviceName, services);
 			replyMessage = new CommunicationMessage(
 					MessageType.ReplyToServer, "Add service Successfully!");
@@ -69,13 +73,17 @@ public class RegistryServer {
 		} else {    // new service with same name
 			// check if this service has already on this server, if it is, then ignore this service.
 			services = remoteObjectTable.get(serviceName);
-			if (services.containsKey(nodeID)) {
+			if (services.containsKey(nodeID) && objectKey == -1) {
 				replyMessage = new CommunicationMessage(
 						MessageType.ReplyToServer, "Add failed: Duplicate services on the same server");
 //System.out.println("Add failed: duplicate service on the same server");
 			} else {
-				services = new ConcurrentHashMap<NodeID, Integer>();
-				services.put(nodeID, 0);    // Initialize the objectKey to 0
+				services = new ConcurrentHashMap<NodeID, Long>();
+				if (objectKey != -1) {   // already new a object on dispatch server and return the object key
+					services.put(nodeID, objectKey);
+				} else {
+					services.put(nodeID, 0L);    // Initialize the objectKey to 0
+				}
 				replyMessage = new CommunicationMessage(
 						MessageType.ReplyToServer, "Add service Successfully!");
 //System.out.println("Add service successfully!");
@@ -97,7 +105,7 @@ public class RegistryServer {
 			return null;
 		} else {
 			// the lists of server running this service
-			Map<NodeID, Integer> serverList = remoteObjectTable.get(serviceName);
+			Map<NodeID, Long> serverList = remoteObjectTable.get(serviceName);
 			// randomly choose one to get better load balancing
 			Random random = new Random();
 			List<NodeID> nodeList = new ArrayList<NodeID>(serverList.keySet());
@@ -106,7 +114,7 @@ public class RegistryServer {
 			}
 			NodeID nodeID = nodeList.get(random.nextInt(serverList.size()));
 			// increase the objectKey 
-			int objectKey = serverList.get(nodeID) + 1;
+			long objectKey = serverList.get(nodeID) + 1;
 			serverList.put(nodeID, objectKey);
 			ror = new RemoteObjectReference(
 					nodeID.getHostName(), nodeID.getPort(), serviceName);
